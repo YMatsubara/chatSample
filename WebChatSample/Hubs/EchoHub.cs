65dce9e8b6fb8f5cfd154859b6b34b84490ce0c5
Y.Matsubara <matsuni.matsuni@gmail.com>
2017-02-26 10:55:05 +0900

    初期コミット

---
+++ WebChatSample/Hubs/EchoHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebChatSample.Hubs
+{
+    [HubName("Echo")]
+    public class EchoHub : Hub
+    {
+        public void Send(String text)
+        {
+            Clients.All.Receive(text);
+
+        }
+    }
+}
---
+++ WebChatSample/Startup1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebChatSample.Startup1))]
+
+namespace WebChatSample
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // アプリケーションの設定方法の詳細については、http://go.microsoft.com/fwlink/?LinkID=316888 を参照してください
+        }
+    }
+}
